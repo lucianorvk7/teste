acd4010ef0ea85f5b097d7a4391b1807dd64374b
lucianorvk7 <luciano.costantino@gmail.com>
2021-07-11 02:20:20 -0300

    novos arquivos adicionados

---
+++ EP1_1 - Alo, mundo!.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 26 18:09:56 2021
+
+@author: Luciano Constantino RA:11201921468
+
+Escreva um programa que mostre a mensagem "Alo, mundo!" (observe que não há acentuação na mensagem).
+"""
+print("Alo, mundo!")
